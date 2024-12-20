4b8805d2d16102545cd8c5915e8d012b523bb7c0
changjunkun <57975614+changjunkun@users.noreply.github.com>
2024-12-20 15:03:29 +0800

    Update deploy-prepra-one.sh

--- deploy-prepra-one.sh
+++ deploy-prepra-one.sh
@@ -1,18 +1,38 @@
 #!/bin/bash
-#git clone https://github.com/changjunkun/aws.git
-cd /root/cjk/aws
-#升级pip，npm版本为 pip:24.3.1  npm：11.0.0
 
+# 检查是否提供了参数
+if [ -z "$1" ]; then
+    echo "No parameter provided. Exiting..."
+    exit 1
+fi
+
+# 获取传递的参数
+MAP=$1
+
+# 输出接收到的参数
+echo "Received parameter: $MAP"
+
+# 执行部署逻辑（根据参数执行不同操作）
+echo "Deploying with MAP=$MAP"
+
+# git clone https://github.com/changjunkun/aws.git
+cd /root/cjk/aws
+
+# 升级pip，npm版本为 pip:24.3.1  npm：11.0.0
 /root/.venv/bin/python3 -m pip install --upgrade pip
 pip --version
+
 npm install -g npm@11.0.0 --force
-#检查npm是否升级到11.0.0版本
+# 检查npm是否升级到11.0.0版本
 npm --version 
-#安装ckd工具
+
+# 安装cdk工具
 sudo npm install -g aws-cdk
+
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
-#若是部署某几个区域
 
+# 若是部署某AWS所有打开的区域
 ./deploy-one.sh $MAP
+
